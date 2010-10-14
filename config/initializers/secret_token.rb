ba3e1f3e3c61e860558ab0994888c99e884fd206
Chris Kimpton <chris@kimptoc.net>
2010-10-14 21:56:11 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BingoCaller::Application.config.secret_token = 'f7fb3130ac6a27ac869756c1aa1c6359ba70ab0816df433fffaa4ad49fe90c69270cab532a83df0d2544fa64b55771a6358e68f20367f1ae1a2e9df2a6fc2190'
