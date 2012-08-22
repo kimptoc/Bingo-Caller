e483927c999baecfdbdc510d69bea3b18e1d49b7
Chris Kimpton <chris@kimptoc.net>
2012-08-22 06:34:47 +0100

    update gems, notes, smaller font for MBA11 and fix logging

--- config/initializers/logger_formatter.rb
+++ config/initializers/logger_formatter.rb
@@ -1,4 +1,32 @@
-module ActiveSupport
+class Logger::SimpleFormatter
+  def call(severity, time, progname, msg)
+
+    level = {
+      "DEBUG" => "DBG",
+      "INFO" => "INF",
+      "WARN" => "WRN",
+      "ERROR" => "ERR",
+      "FATAL" => "FTL"
+    }[severity] || "???"
+
+    @threads = {} unless @threads.present?
+
+    threadName = @threads[Thread.current]
+    unless threadName.present?
+      @threads[Thread.current] = "THD"+ (@threads.size+1).to_s
+      threadName = @threads[Thread.current]
+    end
+
+    "%s:%s:%s>%s\n" % [      time.strftime("%Y%m%d %H:%M:%S.%L") ,
+                              level,
+                              threadName,
+                                   msg]
+
+    #"[#{severity}] #{msg}\n"
+  end
+end
+
+module ActiveSupportX
   class BufferedLogger
 
     def add(severity, message = nil, progname = nil, &block)
@@ -13,17 +41,17 @@ module ActiveSupport
         4 => "FTL"
       }[severity] || "???"
 
-      #@threads = {} unless @threads.present?
-      #
-      #threadName = @threads[Thread.current]
-      #unless threadName.present?
-      #  @threads[Thread.current] = "THD"+ (@threads.size+1).to_s
-      #  threadName = @threads[Thread.current]
-      #end
+      @threads = {} unless @threads.present?
 
-      message = "%s:%s: %s>%s" % [Utils::ThreadHelper.name,
+      threadName = @threads[Thread.current]
+      unless threadName.present?
+        @threads[Thread.current] = "THD"+ (@threads.size+1).to_s
+        threadName = @threads[Thread.current]
+      end
+
+      message = "%s:%s: %s>%s" % [threadName,
                                        level,
-                                     Time.now.strftime("%H:%M:%S"),
+                                     Time.now.strftime("%Y%m%d %H:%M:%S.%L"),
                                      message]
 
       message = "#{message}\n" unless message[-1] == ?\n
